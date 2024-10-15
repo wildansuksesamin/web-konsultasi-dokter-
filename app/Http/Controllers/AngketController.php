4161b7a10088c9716d48a59ad7a3fc0a9749b2cb
wildansuksesamin <wildanfirdausss007@gmail.com>
2024-10-15 23:41:14 +0700

    Fix line endings in composer.lock

---
+++ app/Http/Controllers/AngketController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Angket;
+use Illuminate\Http\Request;
+
+class AngketController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama_siswa' => 'required|string|max:255',
+            'kelas' => 'required|string|max:50',
+            'jawaban_angket' => 'required|string',
+        ]);
+
+        $angket = Angket::create($validatedData);
+
+        return response()->json($angket, 201);
+    }
+
+    public function index()
+    {
+        return Angket::all();
+    }
+}
